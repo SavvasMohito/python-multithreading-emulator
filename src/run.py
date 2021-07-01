0cc287d15c4e1318051324ff838845efc1b3fb7c
Savvas Mantzouranidis <mohito6@hotmail.com>
2021-07-01 13:46:22 +0300

    improved code structure by adding functions

--- src/run.py
+++ src/run.py
@@ -1,14 +1,51 @@
 import json
+import logging
+from pymongo import MongoClient
 
 from components.supervisor import Supervisor
 
-# Read configuration attributes from the config.json file
-file = open('config/config.json', 'r')
-config = json.load(file)
-file.close()
 
-args = [config["devices"], config["url"],
-        config["device_name"], config["access_token"], config["refresh_token"], config["delay"]]
+def get_arguments():
+    args = False
+    try:
+        # Read configuration attributes from the config.json file
+        file = open('config/config.json', 'r')
+        config = json.load(file)
+        file.close()
 
-supervisor = Supervisor(*args)
-supervisor.start()
+        # access_token = get_access_token(config["user_id"])
+        # if access_token is not False:
+        args = [config["devices"], config["url"],
+                config["device_name"], config["access_token"], config["refresh_token"], config["delay"]]
+    except Exception:
+        logging.info(Exception)
+
+    return args
+
+
+def get_access_token(user_id):
+    # TODO make a mongo connection
+    # retrieve access_token based on given user_id
+    # return access_token value
+    access_token = False
+
+    try:
+        client = MongoClient('172.24.1.5', 27017, serverSelectionTimeoutMS=10,
+                             connectTimeoutMS=20000)
+        # specify users collection
+        # search for users.userID = user_id in the collection
+        # set access_token = found user's access_token
+    except Exception:
+        logging.info(Exception)
+
+    return access_token
+
+
+def main():
+    if get_arguments() is not False:
+        supervisor = Supervisor(*get_arguments())
+        supervisor.start()
+
+
+if __name__ == "__main__":
+    main()
