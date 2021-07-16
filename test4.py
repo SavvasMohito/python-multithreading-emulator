124c8a5665b499de2298742ba84dda24b1f8ec05
Savvas Mantzouranidis <mohito6@hotmail.com>
2021-07-16 18:09:37 +0300

    added dynamic client credentials load

--- test4.py
+++ test4.py
@@ -1,11 +1,15 @@
 import requests
+from requests.auth import HTTPBasicAuth
 from urllib.request import urlretrieve
-from requests_oauthlib import OAuth2Session
 from urllib.request import urlretrieve
 from urllib.parse import parse_qs
-import os 
+import json
+import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+file = open('/var/lib/cert-storage/hydraClient.json', 'r')
+clientinfo = json.load(file)
+file.close()
 
 code_url = "https://172.24.1.14/hydra/oauth2/auth?client_id=cc-client&redirect_uri=https://172.24.1.14/emulator/callback&response_type=code&scope=offline_access&state=aW90LTVnLWNyZXc"
 urlretrieve("http://172.24.1.14/download", "cert.pem")
@@ -14,21 +18,15 @@ response = requests.get(code_url, verify="cert.pem")
 queryElements=parse_qs(response.text)
 code=queryElements['/callback?code'][0]
 print(code)
-client_id = 'cc-client'
-client_secret = '0~Ddb0H4QmmWTFJJX6rGE12SNA'
+
+client_id = clientinfo["confClient"]["client_id"]
+client_secret = clientinfo["confSecret"]["client_secret"]
 redirect_uri = 'https://172.24.1.14/emulator/callback'
 scope = ["offline_access"]
 
-# oauth = OAuth2Session(client_id, redirect_uri=redirect_uri,
-#                           scope=scope)
-
-# token = oauth.fetch_token(
-#         'https://172.24.1.14/hydra/oauth2/token',
-#         authorization_response="https://172.24.1.14/emulator/callback", code=code, verify=False) 
 
 token_url = "https://172.24.1.14/hydra/oauth2/token"
 payload='grant_type=authorization_code&redirect_uri=https://172.24.1.14/emulator/callback&code={}'.format(code)
-headers = {'Content-Type': 'application/x-www-form-urlencoded',
-						'Authorization': 'Basic Y2MtY2xpZW50OjB+RGRiMEg0UW1tV1RGSkpYNnJHRTEyU05B'}
-response = requests.post(token_url, data=payload, headers=headers, verify="cert.pem")
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+response = requests.post(token_url, auth=HTTPBasicAuth(client_id, client_secret), data=payload, headers=headers, verify="cert.pem")
 print(response.text)
