f9ad6231e92381ce653ff0b72f4deffb8365a421
Savvas Mantzouranidis <mohito6@hotmail.com>
2021-07-21 19:37:46 +0300

    metrics for msg latency and downtime on token exp

--- src/components/device.py
+++ src/components/device.py
@@ -29,13 +29,34 @@ class Device(threading.Thread):
         headers = {'Content-Type': 'application/json',
                    'Authorization': 'Bearer {}'.format(self._access_token)}
         try:
+            msg_start = time.time()
+
             response = requests.post(
                 "{}{}".format(self._url, "/v2/entities"), data=json.dumps(body), headers=headers, verify="cert.pem")
+            
+            if (response.status_code == 200):
+                msg_end = time.time()
+                msg_time = '{0:.5f}'.format(msg_end - msg_start)
+                print("Message successfully sent in {} seconds.".format(msg_time))
+
             if (response.status_code == 403):
-                # handle token expiration
+                print("fail")
                 response = requests.post(
-                    "{}{}".format(self._url, "/getNewToken"), data=json.dumps(body), headers=headers, verify="cert.pem")
-                self._access_token = response.text
+                        "{}{}".format(self._url, "/getNewToken"), data=json.dumps(body), headers=headers, verify="cert.pem")
+                
+                # handle token expiration and measure downtime
+                down_start = time.time()
+                while (True):
+                    print("nedhos")
+                    response = requests.post(
+                        "{}{}".format(self._url, "/getNewToken"), data=json.dumps(body), headers=headers, verify="cert.pem")
+                    if (self._access_token != response.text):
+                        self._access_token = response.text
+                        down_end = time.time()
+                        down_time = '{0:.5f}'.format(down_end - down_start)
+                        print("Device was down for {} seconds.".format(down_time))
+                        break
+
         except(Exception):
             logger.info(Exception)
 
