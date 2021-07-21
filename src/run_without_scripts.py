69585bb80a4a6fd67cd330c47497a8919a0a1a72
Savvas Mantzouranidis <mohito6@hotmail.com>
2021-07-21 19:36:51 +0300

    added run emulator without scripts option

---
+++ src/run_without_scripts.py
@@ -0,0 +1,33 @@
+import json
+import logging
+
+from components.supervisor import Supervisor
+
+def get_arguments():
+    args = False
+    try:
+        # Read configuration attributes from the config.json file
+        file = open('config/config.json', 'r')
+        config = json.load(file)
+        file.close()
+        args = [
+            config["users"],
+            config["devices"], 
+            config["url"],
+            config["device_name"], 
+            config["delay"],
+        ]
+    except Exception:
+        logging.info(Exception)
+
+    return args
+
+def main():
+    # Setup supervisor for all devices for all users 
+    if get_arguments() is not False:
+        print("Starting Emulation!")
+        supervisor = Supervisor(*get_arguments())
+        supervisor.start()
+        
+if __name__ == "__main__":
+    main()
