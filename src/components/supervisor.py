ce7a7d2b8351abbc40e2280754627491b1edd12f
Antonis Vafeas <antonis.vafeas@bristol.ac.uk>
2021-07-29 16:15:54 +0000

    separated emulator config

--- src/components/supervisor.py
+++ src/components/supervisor.py
@@ -62,11 +62,13 @@ class Supervisor(object):
         # for j in range (self._nusers):
         userCredentials = []
         j = 0
+        # TODO change the config file to config/remoteEmulatorUsers.json
         with open('config/registeredUsers.json', 'r') as infile:
             userCredentials = json.load(infile)
         for user_identity in userCredentials:
             user_id = user_identity["user_id"]
             create_user_metrics_folder(user_id)
+            # TODO change self.get_access_token(user_id) to user_identity["user_token"]
             self._device_kwargs["access_token"] = self.get_access_token(user_id)
             for i in range(self._ndevices):
                 # TODO: Maybe implement random delay for each device
--- src/scripts/login_get_access_tokens.py
+++ src/scripts/login_get_access_tokens.py
@@ -45,6 +45,8 @@ except ServerSelectionTimeoutError:
   exit()
 dbCollection = localClient["iotUsers"]["users"]   
 
+storeCredentials = []
+
 for user_identity in userCredentials:
   
   # Get Login Flow from Kratos
@@ -83,6 +85,13 @@ for user_identity in userCredentials:
 
   token = json.loads(response.text)
   
+  # Add registered user in list
+  storeCredentials.append({
+    "user_token":token["access_token"],
+    "user_id":json_res['identity']['id']
+  })
+
+
   # Add users and their tokens in mongo
   userEntry = {}
   userFilter={"id":  user_identity["user_id"]}
@@ -98,3 +107,7 @@ for user_identity in userCredentials:
       )
   
 localClient.close()
+
+# End of registration. Store users in file
+with open('config/remoteEmulatorUsers.json', 'w') as outfile:
+    json.dump(storeCredentials,outfile, indent=2, sort_keys=True)
